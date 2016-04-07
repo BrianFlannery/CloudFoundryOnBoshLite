3ce649356dc4269a83dcc28728a9bd5082f807a5
Brian Flannery <bflannery@caci.com>
2016-04-07 13:53:47 -0600

    Updates to go.sh and .gitignore to get to the last successful step.

--- go.sh
+++ go.sh
@@ -15,6 +15,12 @@ main() {
   check_vagrant ;
   check_vbox ;
   check_boshLite ;
+  
+  go_bosh_vagrant ;
+  
+  check_cfRelease ;
+
+  go_deploy_cf ;
 }
 
 check_vagrant(){
@@ -31,7 +37,42 @@ check_vbox(){
   #
 }
 check_boshLite() {
-  [[ -e submod ]]
+  [[ -e submod ]] || mkdir submod ;
+  [[ -e submod/bosh-lite ]] || git submodule update ;
+  [[ -e submod/bosh-lite ]] || die "ERROR: Failed to find bosh-lite submodule." ;
+}
+go_bosh_vagrant() {
+  execute cd submod/bosh-lite ;
+  execute vagrant up --provider=virtualbox ;
+  execute bosh target 192.168.50.4 lite ;
+  execute bin/add-route ;
+  execute cd ../.. ;
+}
+check_cfRelease() {
+  [[ -e submod ]] || mkdir submod ;
+  [[ -e submod/cf-release ]] || git submodule update ;
+  [[ -e submod/cf-release ]] || die "ERROR: Failed to find cf-release submodule." ;
+}
+go_deploy_cf() {
+  execute cd submod/bosh-lite ;
+  local f=latest-bosh-lite-stemcell-warden.tgz ;
+  if [[ ! -e $f ]] ; then
+    for path in .. ../.. ; do
+      if [[ -e $path/$f ]] ; then
+        ln -s $path/$f $f ;
+        break ;
+      fi ;
+    done ;
+  fi ;
+  execute bin/provision_cf ;
+  
+  # # # Dies with this conclusion:
+    # Generating bosh lite manifest from /Users/bflannery/dw/github/cf/CloudFoundryOnBoshLite/submod/cf-release/scripts/generate-bosh-lite-dev-manifest
+    # Aborted. Please install spiff by following https://github.com/cloudfoundry-incubator/spiff#installation
+    # ERROR: From command bin/provision_cf: '1'.
+  # TODO FIXME XXX
+
+  execute cd ../.. ;
 }
 
 main ;
